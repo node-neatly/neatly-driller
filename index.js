9ba0f643afe013f5d9b0a270c37fc5a869cb69aa
Christian Blaschke <mail@platdesign.de>
2016-08-01 03:59:59 +0200

    use new bootstrap points

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports = function(neatly) {
 	return {
 		wrap: function(module, mocks) {
 			return function bootsrap(configHandler) {
-				let _module = neatly.module('wrapped', [module, ...mocks]);
+				let _module = neatly.module('wrapped', [module, ...mocks||[]]);
 				if(configHandler) {
 					_module.config(configHandler);
 				}
-				return neatly.bootsrap(_module);
+				return neatly.init(_module);
 			};
 		}
 	};
